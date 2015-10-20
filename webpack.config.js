5101646f543ed2dc56738e79903bbb3c9eed241a
gdi2290 <github@gdi2290.com>
2015-10-20 09:06:43 +0100

    chore: init webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  entry: './src/bootstrap',
+  output: {
+    path: __dirname + '/__build__',
+    publicPath: '__build__/',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.ts']
+  },
+  module: {
+    loaders: [{
+      test: /\.ts/, loaders: ['ts'], exclude: /node_modules/
+    }]
+  }
+};
