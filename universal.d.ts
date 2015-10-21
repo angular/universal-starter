250df6db53e43b5b8528a79f1f5aeeb6d484eb87
gdi2290 <github@gdi2290.com>
2015-10-21 05:28:25 +0100

    fix: install problems

--- universal.d.ts
+++ universal.d.ts
@@ -1,7 +1,7 @@
 declare module "angular2-universal-preview" {
   function ng2engineWithPreboot(): any;
   function ng2engine(): any;
-  function bootstrap(component: any, providers: any): any;
+  function bootstrap(component: any, providers?: any): any;
   var BASE_URL: any;
   var PRIME_CACHE: any;
   var HTTP_PROVIDERS: Array<any>;
