5ac040c9a1037a320e7432fd3b7f6e608d0a5037
gdi2290 <github@gdi2290.com>
2016-01-07 23:22:28 -0800

    chore(tsd): correct typings

--- universal.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "angular2-universal-preview" {
-  function ng2engineWithPreboot(): any;
-  function ng2engine(): any;
-  function bootstrap(component: any, providers?: any): any;
-  var BASE_URL: any;
-  var PRIME_CACHE: any;
-  var HTTP_PROVIDERS: Array<any>;
-}
