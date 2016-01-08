aab6ac01588e6c6cdb6f8255c65a9fdb17f6392c
gdi2290 <github@gdi2290.com>
2016-01-07 23:22:47 -0800

    refactor(webpack.config): handle both client/server

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,69 @@
+var webpackMerge = require('webpack-merge');
+var path = require('path');
 
-module.exports = {
-  target: 'web',
-  entry: './src/bootstrap',
+var common = {
+  context: process.cwd(),
   output: {
-    path: __dirname + '/__build__',
-    publicPath: '__build__/',
+    publicPath: path.resolve(__dirname),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['', '.js', '.ts']
+    root: __dirname + '/src',
+    extensions: ['', '.ts', '.json', '.js']
   },
   module: {
-    loaders: [{
-      test: /\.ts/, loaders: ['ts'], exclude: /node_modules/
-    }]
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        query: {
+          'ignoreDiagnostics': [
+            2403, // 2403 -> Subsequent variable declarations
+            2420, // 2420 -> Class incorrectly implements interface
+            2300, // 2300 -> Duplicate identifier
+            2374, // 2374 -> Duplicate number index signature
+            2375  // 2375 -> Duplicate string index signature
+          ]
+        },
+        exclude: [ /node_modules/ ]
+      }
+    ]
   }
 };
+
+var client = {
+  target: 'web',
+  entry: './src/client',
+  output: {
+    path: __dirname + '/dist/client'
+  }
+};
+
+var server = {
+  target: 'node',
+  entry: './src/server',
+  output: {
+    path: __dirname + '/dist/server'
+  },
+  externals: function checkNodeImport(context, request, cb) {
+    if (!path.isAbsolute(request) && request.charAt(0) !== '.') {
+      cb(null, 'commonjs ' + request); return;
+    }
+    cb();
+  },
+  node: {
+    global: true,
+    __dirname: true,
+    __filename: true,
+    process: true,
+    Buffer: true
+  }
+};
+
+module.exports = [
+  // Client
+  webpackMerge({}, common, client),
+
+  // Server
+  webpackMerge({}, common, server)
+]
