745d4d10c2faae5f98f5b6743c07f19faadef514
gdi2290 <github@gdi2290.com>
2016-01-07 23:23:44 -0800

    refactor(app): move to app folder

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
 import {bootstrap} from 'angular2/platform/browser';
 // import {bootstrap} from 'angular2-universal-preview';
 
-import {App} from './app';
+import {App} from './app/app';
 
 bootstrap(App);
