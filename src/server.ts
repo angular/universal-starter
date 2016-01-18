eec51668574bd16a59388103c22db1b4b667696e
gdi2290 <github@gdi2290.com>
2016-01-17 22:55:50 -0800

    feat(server): html5 routes, router, prod

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
 import * as path from 'path';
 import * as express from 'express';
-import * as universal from 'angular2-universal-preview';
+import universal, {
+  BASE_URL,
+  SERVER_LOCATION_PROVIDERS
+} from 'angular2-universal-preview';
 
 // Angular 2
+import {provide, enableProdMode} from 'angular2/core';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 import {App} from './app/app';
 
 let app = express();
 let root = path.join(path.resolve(__dirname, '..'));
+
+enableProdMode();
 
 // Express View
 app.engine('.html', universal.ng2engine);
@@ -17,8 +24,16 @@ app.set('view engine', 'html');
 app.use(express.static(root));
 
 // Routes
-app.use('/', (req, res) => {
-  res.render('index', { App });
+app.use('/*', (req, res) => {
+  res.render('index', {
+    App,
+    providers: [
+      ROUTER_PROVIDERS,
+      provide(BASE_URL, {useValue: 'http://localhost:3000' + req.originalUrl}),
+      SERVER_LOCATION_PROVIDERS,
+    ],
+    preboot: true
+  });
 });
 
 // Server
