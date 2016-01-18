0947c9e33c3e6c33b8e162a163432ed5e935bddb
gdi2290 <github@gdi2290.com>
2016-01-17 22:54:45 -0800

    feat(client): include preboot with routes

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,9 @@
-import {bootstrap} from 'angular2/platform/browser';
-// import {bootstrap} from 'angular2-universal-preview';
+// import {bootstrap} from 'angular2/platform/browser';
+import {bootstrap} from 'angular2-universal-preview';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 
 import {App} from './app/app';
 
-bootstrap(App);
+bootstrap(App, [
+  ...ROUTER_PROVIDERS
+]);
