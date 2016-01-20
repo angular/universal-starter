166e33c91ec6e3ecccbe1042a17340fecc0ebae4
gdi2290 <github@gdi2290.com>
2016-01-19 17:29:06 -0800

    feat: include system.config

---
+++ system.config.js
@@ -0,0 +1,9 @@
+System.config({
+  defaultJSExtensions: true,
+  map: {
+    'typescript': 'node_modules/typescript/lib/typescript.js',
+    'angular2': 'node_modules/angular2',
+    'angular2-universal-preview': 'node_modules/angular2-universal-preview/dist/client/index',
+    'rxjs': 'node_modules/rxjs'
+  }
+});
