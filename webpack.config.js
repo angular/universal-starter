cfb7b52b12e1da9de7ee7c2a452e7636c93aaa09
gdi2290 <github@gdi2290.com>
2016-01-19 18:58:56 -0800

    chore: update tsd to typings

--- webpack.config.js
+++ webpack.config.js
@@ -10,15 +10,6 @@ var common = {
       {
         test: /\.ts$/,
         loader: 'ts-loader',
-        query: {
-          'ignoreDiagnostics': [
-            2403, // 2403 -> Subsequent variable declarations
-            2420, // 2420 -> Class incorrectly implements interface
-            2300, // 2300 -> Duplicate identifier
-            2374, // 2374 -> Duplicate number index signature
-            2375  // 2375 -> Duplicate string index signature
-          ]
-        },
         exclude: [ /node_modules/ ]
       }
     ]
