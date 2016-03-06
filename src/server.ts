8abc4f5e0ef97b7c1f109c75907a82dc0adb92a0
gdi2290 <github@gdi2290.com>
2016-03-05 23:07:22 -0800

    chore(server): update api

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import * as path from 'path';
 import * as express from 'express';
 
 // Angular 2
-import {ng2engine, REQUEST_URL, NODE_LOCATION_PROVIDERS} from 'angular2-universal-preview';
+import {expressEngine, REQUEST_URL, NODE_LOCATION_PROVIDERS} from 'angular2-universal-preview';
 import {provide, enableProdMode} from 'angular2/core';
 import {APP_BASE_HREF, ROUTER_PROVIDERS} from 'angular2/router';
+// Application
 import {App} from './app/app';
+import {Title, ServerOnlyApp} from './server-only-app/server-only-app';
 
 let app = express();
 let root = path.join(path.resolve(__dirname, '..'));
@@ -13,22 +15,23 @@ let root = path.join(path.resolve(__dirname, '..'));
 enableProdMode();
 
 // Express View
-app.engine('.html', ng2engine);
+app.engine('.html', expressEngine);
 app.set('views', __dirname);
 app.set('view engine', 'html');
+
 
 function ngApp(req, res) {
   let baseUrl = '/';
   let url = req.originalUrl || '/';
   res.render('index', {
-    App,
+    directives: [App, Title, ServerOnlyApp],
     providers: [
       provide(APP_BASE_HREF, {useValue: baseUrl}),
       provide(REQUEST_URL, {useValue: url}),
       ROUTER_PROVIDERS,
       NODE_LOCATION_PROVIDERS,
     ],
-    preboot: true
+    preboot: false
   });
 }
 
