76a4707bb0bec6ecdd5b0e9bff88649485308dd8
gdi2290 <github@gdi2290.com>
2016-03-05 23:06:25 -0800

    refactor(webpack.conf): path.join and plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,37 @@
 var webpackMerge = require('webpack-merge');
+var webpack = require('webpack');
 var path = require('path');
 
-var common = {
+var commonConfig = {
   resolve: {
     extensions: ['', '.ts', '.js']
   },
   module: {
     loaders: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        exclude: [ /node_modules/ ]
-      }
+      // TypeScript
+      { test: /\.ts$/, loader: 'ts-loader', exclude: [ /node_modules/ ] }
     ]
-  }
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(true)
+  ]
 };
 
-var client = {
+
+var clientConfig = {
   target: 'web',
   entry: './src/client',
   output: {
-    path: __dirname + '/dist/client'
+    path: path.join(__dirname, 'dist', 'client')
   }
 };
 
-var server = {
+
+var serverConfig = {
   target: 'node',
   entry: './src/server',
   output: {
-    path: __dirname + '/dist/server'
+    path: path.join(__dirname, 'dist', 'server')
   },
   externals: checkNodeImport,
   node: {
@@ -40,10 +43,19 @@ var server = {
   }
 };
 
-var defaults = {
+
+
+// Default config
+var defaultConfig = {
+  module: {
+    noParse: [
+      path.join(__dirname, 'zone.js', 'dist'),
+      path.join(__dirname, 'angular2', 'bundles')
+    ]
+  },
   context: __dirname,
   resolve: {
-    root: __dirname + '/src'
+    root: path.join(__dirname, '/src')
   },
   output: {
     publicPath: path.resolve(__dirname),
@@ -51,18 +63,21 @@ var defaults = {
   }
 }
 
+
+
+
 module.exports = [
   // Client
-  webpackMerge({}, defaults, common, client),
+  webpackMerge({}, defaultConfig, commonConfig, clientConfig),
 
   // Server
-  webpackMerge({}, defaults, common, server)
+  webpackMerge({}, defaultConfig, commonConfig, serverConfig)
 ]
 
 // Helpers
 function checkNodeImport(context, request, cb) {
-    if (!path.isAbsolute(request) && request.charAt(0) !== '.') {
-      cb(null, 'commonjs ' + request); return;
-    }
-    cb();
+  if (!path.isAbsolute(request) && request.charAt(0) !== '.') {
+    cb(null, 'commonjs ' + request); return;
   }
+  cb();
+}
