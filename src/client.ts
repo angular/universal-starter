535ed1a4558d9d50aafad30839e85c853fb4ce38
gdi2290 <github@gdi2290.com>
2016-03-07 11:23:58 -0800

    style(client): remove comment

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,6 @@
-// import {bootstrap} from 'angular2/platform/browser';
 import 'angular2-universal-preview/polyfills';
 import {prebootComplete} from 'angular2-universal-preview';
+
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
 
