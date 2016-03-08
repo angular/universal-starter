94c064667ad4233c376f647ff7853b7a2bb37b56
gdi2290 <github@gdi2290.com>
2016-03-07 16:47:43 -0800

    refactor(gulpfile): prerender

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,18 +3,19 @@ import 'angular2-universal-preview/polyfills';
 import {REQUEST_URL, NODE_LOCATION_PROVIDERS} from 'angular2-universal-preview';
 import {provide, enableProdMode} from 'angular2/core';
 import {APP_BASE_HREF, ROUTER_PROVIDERS} from 'angular2/router';
+import {prerender} from 'angular2-gulp-prerender';
 
-var ngPreRender = require('angular2-gulp-prerender');
 
 import {App} from './src/app/app';
+import {Title, ServerOnlyApp} from './src/server-only-app/server-only-app';
 
 enableProdMode();
 
 gulp.task('prerender', () => {
 
   return gulp.src('./src/index.html')
-    .pipe(ngPreRender({
-      App,
+    .pipe(prerender({
+      directives: [ App, Title, ServerOnlyApp ],
       providers: [
         provide(APP_BASE_HREF, {useValue: '/'}),
         provide(REQUEST_URL, {useValue: '/'}),
