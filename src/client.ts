0774554ce0ffb987deb1fc6454e81577b2ab106e
gdi2290 <github@gdi2290.com>
2016-03-26 22:31:48 -0700

    feat(client): prod

--- src/client.ts
+++ src/client.ts
@@ -3,8 +3,11 @@ import {prebootComplete} from 'angular2-universal-preview';
 
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
+import {enableProdMode} from 'angular2/core';
 
 import {App} from './app/app.component';
+
+enableProdMode();
 
 bootstrap(App, [
   ...ROUTER_PROVIDERS
