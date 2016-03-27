35172d63d06dd641fd33a0680704068d8a8fb2f7
gdi2290 <github@gdi2290.com>
2016-03-26 23:14:49 -0700

    feat(client): http providers

--- src/client.ts
+++ src/client.ts
@@ -3,6 +3,7 @@ import {prebootComplete} from 'angular2-universal-preview';
 
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS} from 'angular2/router';
+import {HTTP_PROVIDERS} from 'angular2/http';
 import {enableProdMode} from 'angular2/core';
 
 import {App} from './app/app.component';
@@ -10,6 +11,7 @@ import {App} from './app/app.component';
 enableProdMode();
 
 bootstrap(App, [
-  ...ROUTER_PROVIDERS
+  ...ROUTER_PROVIDERS,
+  ...HTTP_PROVIDERS
 ])
 .then(prebootComplete);
