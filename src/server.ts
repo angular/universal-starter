d5af703ecb16cfb1e9b5abd80c826957ff69a640
PatrickJS <github@gdi2290.com>
2016-06-09 08:27:01 -0700

    refactor(server): split into two files

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+// the polyfills must be the first thing imported in node.js
 import 'angular2-universal/polyfills';
 
 import * as path from 'path';
@@ -5,17 +6,7 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 
 // Angular 2 Universal
-import {
-  provide,
-  enableProdMode,
-  expressEngine,
-  REQUEST_URL,
-  ORIGIN_URL,
-  BASE_URL,
-  NODE_ROUTER_PROVIDERS,
-  NODE_HTTP_PROVIDERS,
-  ExpressEngineConfig
-} from 'angular2-universal';
+import {enableProdMode, expressEngine} from 'angular2-universal';
 
 // Application
 import {App} from './app/app.component';
@@ -23,6 +14,7 @@ import {App} from './app/app.component';
 const app = express();
 const ROOT = path.join(path.resolve(__dirname, '..'));
 
+// enable prod for faster renders
 enableProdMode();
 
 // Express View
@@ -32,66 +24,25 @@ app.set('view engine', 'html');
 
 app.use(bodyParser.json());
 
-
-function ngApp(req, res) {
-  let baseUrl = '/';
-  let url = req.originalUrl || '/';
-
-  let config: ExpressEngineConfig = {
-    directives: [ App ],
-    platformProviders: [
-      provide(ORIGIN_URL, {useValue: 'http://localhost:3000'}),
-      provide(BASE_URL, {useValue: baseUrl}),
-    ],
-    providers: [
-      provide(REQUEST_URL, {useValue: url}),
-      NODE_ROUTER_PROVIDERS,
-      NODE_HTTP_PROVIDERS,
-    ],
-    async: true,
-    preboot: false // { appRoot: 'app' } // your top level app component selector
-  };
-
-  res.render('index', config);
-}
-
-function indexFile(req, res) {
-  res.sendFile('/index.html', {root: __dirname});
-}
-
 // Serve static files
 app.use(express.static(ROOT, {index: false}));
 
 // Our API for demos only
-var USER_ID = 'f9d98cf1-1b96-464e-8755-bcc2a5c09077'; // hardcoded as an example
-var fakeDemoRedisCache = {
-  [USER_ID]: {"data": "This fake data came from the server."}
-};
-// Our API for demos only
-var fakeDataBase = {
-  get() {
-    let res = { data: 'This fake data came from the server.' };
-    return Promise.resolve(res);
-  }
-}
-// Our API for demos only
-app.get('/data.json', (req, res) => {
-  if (fakeDemoRedisCache[USER_ID]) {
-    return res.json(fakeDemoRedisCache[USER_ID]);
-  }
-  
-  fakeDataBase.get()
-    .then(data => {
-      fakeDemoRedisCache[USER_ID] = data
-      return data;
-    })
-    .then(data => res.json(data));
-});
+import {serverApi} from './backend/api';
+app.get('/data.json', serverApi);
 
 // Routes with html5pushstate
+import {ngApp} from './main.node';
 app.use('/', ngApp);
 app.use('/about', ngApp);
 app.use('/home', ngApp);
+
+// use indexFile over ngApp only when there is too much load on the server
+function indexFile(req, res) {
+  // when there is too much load on the server just send
+  // the index.html without prerendering for client-only
+  res.sendFile('/index.html', {root: __dirname});
+}
 
 // Server
 app.listen(3000, () => {
