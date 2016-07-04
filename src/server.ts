aafecbd18d91f75b782123246fe9acff72d3ee11
PatrickJS <github@gdi2290.com>
2016-07-03 20:48:49 -0700

    chore: universal secret

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ app.engine('.html', expressEngine);
 app.set('views', __dirname);
 app.set('view engine', 'html');
 
-app.use(cookieParser());
+app.use(cookieParser('Angular 2 Universal'));
 app.use(bodyParser.json());
 
 // Serve static files
