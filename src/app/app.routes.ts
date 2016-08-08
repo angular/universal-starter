7c49bca9426f8b88b0de58a7ec663b2503df258f
Mark Pieszak <mpieszak84@gmail.com>
2016-08-08 04:22:46 -0400

    chore(package): Update dependencies (#141)
    * chore(dependencies): Update
    
    * Merge remote-tracking branch 'angular/master'
    
    # Conflicts:
    #	package.json
    
    * Add pathMatch for router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Home } from './home';
 import { About } from './app.component';
 
 export const routes: RouterConfig = [
-  { path: '', redirectTo: 'home' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'about', component: About },
   { path: '**', redirectTo: 'home' }
