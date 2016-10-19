3679594ff91fb585ca5a491ff52190960c37dc67
PatrickJS <github@gdi2290.com>
2016-10-19 13:14:57 -0400

    feat: UniversalCache for hydration

--- src/app.node.module.ts
+++ src/app.node.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { UniversalModule } from 'angular2-universal/node'; // for AoT we need to manually split universal packages
+import { UniversalModule, isBrowser, isNode } from 'angular2-universal/node'; // for AoT we need to manually split universal packages
 
 import { App, Home } from './app/app';
+import { Cache } from './app/universal-cache';
 
 @NgModule({
   bootstrap: [ App ],
@@ -14,8 +15,22 @@ import { App, Home } from './app/app';
     RouterModule.forRoot([
       { path: '', component: Home, pathMatch: 'full' }
     ])
+  ],
+  providers: [
+    { provide: 'isBrowser', useValue: isBrowser },
+    { provide: 'isNode', useValue: isNode },
+    Cache
   ]
 })
 export class MainModule {
+  constructor(public cache: Cache) {
 
+  }
+  // we need to use the arrow function here to bind the context as this is a gotcha in Universal for now until it's fixed
+  universalDoDehydrate = (universalCache) => {
+    universalCache['Cache'] = JSON.stringify(this.cache.dehydrate());
+  }
+  universalAfterDehydrate = () => {
+    this.cache.clear();
+  }
 }
