1af84e8d8c3c6095f6c691a3f2d8aad784dafdaf
Matthias Baumann <matthias@Matthiass-MacBook-Pro.local>
2016-10-29 21:44:15 +0200

    Add .css file to demonstrate style usage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 }
