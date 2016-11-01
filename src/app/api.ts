d44a434fb0c108b07c942509b8d1fc7008a372b3
PatrickJS <github@gdi2290.com>
2016-11-01 14:51:53 -0400

    feat(api.ts): update with model/api services

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,31 +1,69 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/thorw';
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/share';
 
-import { Cache  } from './universal-cache';
+import { CacheService  } from './universal-cache';
+
+export function hashCode(str) {
+  let hash = 0;
+  if (str.length === 0) {
+    return hash;
+  }
+  for (let i = 0; i < str.length; i++) {
+    let char = str.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
 
 @Injectable()
 export class ApiService {
-
-  constructor(public _http: Http, public _cache: Cache) {
+  constructor(public _http: Http) {
 
   }
-  // whatever domain/feature method name
-  getModel(url) {
+
+ /**
+  * whatever domain/feature method name
+  */
+  get(url: string, options?: any) {
+    return this._http.get(url, options)
+      .map(res => res.json())
+      .catch(err => {
+        console.log('Error: ', err);
+        return Observable.throw(err);
+      });
+  }
+
+}
+
+@Injectable()
+export class ModelService {
+
+  constructor(public _api: ApiService, public _cache: Cache) {
+
+  }
+
+ /**
+  * whatever domain/feature method name
+  */
+  get(url) {
     // you want to return the cache if there is a response in it. This would cache the first response so if your API isn't idempotent you probably want to remove the item from the cache after you use it. LRU of 1
     let key = url;
+
     if (this._cache.has(key)) {
       return Observable.of(this._cache.get(key));
     }
     // you probably shouldn't .share() and you should write the correct logic
-    return this._http.get(url)
-      .map(res => res.json())
+    return this._api.get(url)
       .do(json => {
         this._cache.set(key, json);
       })
       .share();
   }
+
 }
