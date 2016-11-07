16a157b8260c9a839e459535ae3e694bde4e60a8
PatrickJS <github@gdi2290.com>
2016-11-06 17:00:38 -0800

    refactor(home): move to method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,10 +13,15 @@ export class HomeComponent {
   data = {};
   constructor(public model: ModelService) {
 
+    // we need the data synchronously for the client to set the server response
+    // we create another method so we have more control for testing
+    this.universalInit();
+  }
+
+  universalInit() {
     this.model.get('/data.json').subscribe(data => {
       this.data = data;
     });
-
   }
 
 }
