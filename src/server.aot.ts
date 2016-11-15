12003694002b250ba2c2edcdb92a11c592fef2a8
PatrickJS <github@gdi2290.com>
2016-11-15 16:38:36 -0500

    feat: client/server aot

---
+++ src/server.aot.ts
@@ -0,0 +1,91 @@
+// the polyfills must be one of the first things imported in node.js.
+// The only modules to be imported higher - node modules with es6-promise 3.x or other Promise polyfill dependency
+// (rule of thumb: do it if you have zone.js exception that it has been overwritten)
+// if you are including modules that modify Promise, such as NewRelic,, you must include them before polyfills
+import 'angular2-universal-polyfills';
+
+// Fix Universal Style
+import { NodeDomRootRenderer, NodeDomRenderer } from 'angular2-universal/node';
+function renderComponentFix(componentProto: any) {
+  return new NodeDomRenderer(this, componentProto, this._animationDriver);
+}
+NodeDomRootRenderer.prototype.renderComponent = renderComponentFix;
+// End Fix Universal Style
+
+import * as path from 'path';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cookieParser from 'cookie-parser';
+
+// Angular 2
+import { enableProdMode } from '@angular/core';
+// Angular 2 Universal
+import { createEngine } from 'angular2-express-engine';
+
+// App
+import { MainModuleNgFactory } from './app/app.node.module.ngfactory';
+
+// enable prod for faster renders
+enableProdMode();
+
+const app = express();
+const ROOT = path.join(path.resolve(__dirname, '..'));
+
+// Express View
+app.engine('.html', createEngine({
+  precompile: false,
+  ngModule: MainModuleNgFactory,
+  providers: [
+    // use only if you have shared state between users
+    // { provide: 'LRU', useFactory: () => new LRU(10) }
+
+    // stateless providers only since it's shared
+  ]
+}));
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname);
+app.set('view engine', 'html');
+
+app.use(cookieParser('Angular 2 Universal'));
+app.use(bodyParser.json());
+
+// Serve static files
+app.use('/assets', express.static(path.join(__dirname, 'assets'), {maxAge: 30}));
+app.use(express.static(path.join(ROOT, 'dist/client'), {index: false}));
+
+
+import { serverApi } from './backend/api';
+// Our API for demos only
+app.get('/data.json', serverApi);
+
+function ngApp(req, res) {
+  res.render('index', {
+    req,
+    res,
+    // time: true, // use this to determine what part of your app is slow only in development
+    preboot: false,
+    baseUrl: '/',
+    requestUrl: req.originalUrl,
+    originUrl: 'http://localhost:3000'
+  });
+}
+// Routes with html5pushstate
+// ensure routes match client-side-app
+app.get('/', ngApp);
+app.get('/about', ngApp);
+app.get('/about/*', ngApp);
+app.get('/home', ngApp);
+app.get('/home/*', ngApp);
+
+
+app.get('*', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  var pojo = { status: 404, message: 'No Content' };
+  var json = JSON.stringify(pojo, null, 2);
+  res.status(404).send(json);
+});
+
+// Server
+let server = app.listen(app.get('port'), () => {
+  console.log(`Listening on: http://localhost:${server.address().port}`);
+});
