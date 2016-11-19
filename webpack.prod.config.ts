34e132d61e12cb89ba69ec1a8eb69a80e83dda0a
anotherjsguy <kuldeepkeshwar@gmail.com>
2016-11-19 18:12:26 +0530

    Update webpack.prod.config.ts

--- webpack.prod.config.ts
+++ webpack.prod.config.ts
@@ -4,7 +4,7 @@ var clone = require('js.clone');
 var webpackMerge = require('webpack-merge');
 var V8LazyParseWebpackPlugin = require('v8-lazy-parse-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-import webpackConfig, { root, checkNodeImport, includeClientPackages } from './webpack.config';
+import webpackConfig, { root,  includeClientPackages } from './webpack.config';
 // var CompressionPlugin = require('compression-webpack-plugin');
 
 
