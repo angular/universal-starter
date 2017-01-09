2ec917839e89acb6d37b9a321c3a479164fc7966
PatrickJS <github@gdi2290.com>
2017-01-09 10:35:49 -0500

    fix(server.aot): onHandleError

--- src/server.aot.ts
+++ src/server.aot.ts
@@ -81,20 +81,25 @@ process.on('uncaughtException', function (err) {
 });
 
 function ngApp(req, res) {
-  res.render('index', {
-    req,
-    res,
-    // time: true, // use this to determine what part of your app is slow only in development
-    preboot: false,
-    baseUrl: '/',
-    requestUrl: req.originalUrl,
-    originUrl: `http://localhost:${ app.get('port') }`,
-    onHandleError: (parentZoneDelegate, currentZone, targetZone, error) => {
-      console.warn('Error in SSR, serving for direct CSR');
-      res.sendFile('index.html', {root: './src'});
-      return false;
-    }
+
+  function onHandleError(parentZoneDelegate, currentZone, targetZone, error)  {
+    console.warn('Error in SSR, serving for direct CSR');
+    res.sendFile('index.html', {root: './src'});
+    return false;
+  }
+
+  Zone.current.fork({ name: 'CSR fallback', onHandleError }).run(() => {
+    res.render('index', {
+      req,
+      res,
+      // time: true, // use this to determine what part of your app is slow only in development
+      preboot: false,
+      baseUrl: '/',
+      requestUrl: req.originalUrl,
+      originUrl: `http://localhost:${ app.get('port') }`
+    });
   });
+
 }
 
 /**
