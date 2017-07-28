75de71885853ebfbed2a56c6d129e55b1a3a16b2
PatrickJS <github@gdi2290.com>
2017-07-28 14:10:17 -0700

    fix: bootstrap on document ready (#406)

--- src/main.browser.ts
+++ src/main.browser.ts
@@ -1,8 +1,14 @@
 import 'zone.js/dist/zone';
 import 'reflect-metadata';
+
 import 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAppModule } from './app/browser-app.module';
 
-platformBrowserDynamic().bootstrapModule(BrowserAppModule);
+export function main() {
+  return platformBrowserDynamic().bootstrapModule(BrowserAppModule);
+}
+
+document.addEventListener('DOMContentLoaded', main, false);
