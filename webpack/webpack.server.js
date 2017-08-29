303bb16db6e05c54165812bdbc91de0ac738b7e4
PatrickJS <github@gdi2290.com>
2017-08-29 13:07:34 -0700

    fix: include nodeExternals for server

--- webpack/webpack.server.js
+++ webpack/webpack.server.js
@@ -1,11 +1,16 @@
 const { root } = require('./helpers');
 
 const { AotPlugin } = require('@ngtools/webpack');
+const nodeExternals = require('webpack-node-externals');
 
 /**
  * This is a server config which should be merged on top of common config
  */
 module.exports = {
+  externals: [nodeExternals({
+    /@angular/,
+    /@ng/
+  })],
   entry: root('./src/main.server.ts'),
   output: {
     filename: 'server.js'
