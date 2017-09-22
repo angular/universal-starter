d318d80463db1ca0145f2b23ed53388cf668b842
Mark Pieszak <mpieszak84@gmail.com>
2017-09-22 19:16:17 -0400

    feat(cli): typescript updates

--- cli/src/main.server.ts
+++ cli/src/main.server.ts
@@ -1,8 +1,1 @@
-import { environment } from './environments/environment';
-import { enableProdMode } from '@angular/core';
-
-if (environment.production) {
-  enableProdMode();
-}
-
 export {AppServerModule} from './app/app.server.module';
