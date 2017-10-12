1f3ee46d72fea97eed5d5c2875fa281407eddc90
Mark Pieszak <mpieszak84@gmail.com>
2017-10-12 19:35:35 -0400

    fix(server.ts): update DIST_FOLDER location

--- cli/server.ts
+++ cli/server.ts
@@ -14,7 +14,7 @@ enableProdMode();
 const app = express();
 
 const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = process.cwd();
+const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // Our index.html we'll use as our template
 const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
