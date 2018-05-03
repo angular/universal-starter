7ccfa1b885c6a8fdf1612430d382d7fde401b0aa
Fabian Wiles <fabianwiles@live.com>
2018-05-04 10:48:06 +1200

    feat(build): upgrade to v6 (#574)
    * feat(build): upgrade to v6
    
    * [squash] update to nguniversal v6

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'none',
   entry: {
     // This is our Express server for Dynamic universal
     server: './server.ts',
