9e2108e0a99edac277dd7acb1d2ff8aeae0e08da
Philip Sultanescu <philip@firstorder.io>
2018-06-01 13:39:36 +0200

    fix(webpack.server.config): Unnecessary escape character: \/ (#599)
    Avoid warnings given by codefactor.io and other linters by refactoring
    the regex using capturing groups like in the lines below.
    
    Linked to #579

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -26,7 +26,7 @@ module.exports = {
       {
         // Mark files inside `@angular/core` as using SystemJS style dynamic imports.
         // Removing this will cause deprecation warnings to appear.
-        test: /[\/\\]@angular[\/\\]core[\/\\].+\.js$/,
+        test: /(\\|\/)@angular(\\|\/)core(\\|\/).+\.js$/,
         parser: { system: true },
       },
     ]
