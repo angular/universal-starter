005fb5eeea017d6ad8a777e6ecd2cf085281fa68
ali <connexion.founder@outlook.com>
2018-07-18 16:40:29 +0200

    Add hmr support

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  template: `<h3>{{ message }}</h3>`
+  template: `<h3>ss{{ message }}</h3>`
 })
 export class HomeComponent implements OnInit {
   public message: string;
