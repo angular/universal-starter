0c46f95ea82d40f047e005dac3df586e66219086
Fabian Wiles <fabianwiles@live.com>
2019-01-01 08:43:26 +1300

    clean up config

--- server.ts
+++ server.ts
@@ -19,7 +19,8 @@ const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
+// NOTE: this will be an error in your console until the universal bundle is built
+import { AppServerModuleNgFactory, LAZY_MODULE_MAP } from './dist/server/main';
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine('html', ngExpressEngine({
